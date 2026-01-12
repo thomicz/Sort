cd0cdd843385bdd2e3ced0faa405a38c0db858af
Tomáš Dvořák <168711786+thomicz@users.noreply.github.com>
2026-01-12 16:13:42 +0100

    Add files via upload

---
+++ InsertionSort/Sort.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace InsertionSort
+{
+    internal class Sort
+    {
+        public T[] SortData<T>(T[] data) where T : IComparable<T>
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            for (int i = 1; i < data.Length; i++)
+            {
+                T key = data[i];
+                int j = i - 1;
+
+                while (j >= 0 && data[j].CompareTo(key) > 0)
+                {
+                    data[j + 1] = data[j];
+                    j--;
+                }
+
+                data[j + 1] = key;
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Time: {sw.Elapsed}");
+
+            return data;
+        }
+    }
+}
