95fa92e04a1b3f79ff80fa2bb1ca78e31cea14cf
Tomáš Dvořák <168711786+thomicz@users.noreply.github.com>
2026-01-12 15:47:54 +0100

    Add files via upload

---
+++ BubbleSort/Sort.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+namespace BubbleSort
+{
+    internal class Sort
+    {
+        public T[] SortData<T>(T[] data) where T : IComparable<T>
+        {
+            int length = data.Length;
+
+            Stopwatch sw = Stopwatch.StartNew();
+
+            while (!IsSorted(data))
+            {
+                for (int i = 0; i < length - 1; i++)
+                {
+                    if (data[i].CompareTo(data[i + 1]) > 0)
+                    {
+                        T temp = data[i];
+                        data[i] = data[i + 1];
+                        data[i + 1] = temp;
+                    }
+                }
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Time: {sw.Elapsed}");
+
+            return data;
+        }
+
+        private bool IsSorted<T>(T[] data) where T : IComparable<T>
+        {
+            for (int i = 0; i < data.Length - 1; i++)
+            {
+                if (data[i].CompareTo(data[i + 1]) > 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
