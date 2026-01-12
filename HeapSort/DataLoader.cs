12d13178d0224b3fb111e10c0ca393b8667e98eb
Tomáš Dvořák <168711786+thomicz@users.noreply.github.com>
2026-01-12 16:04:11 +0100

    Add files via upload

---
+++ HeapSort/DataLoader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapSort
+{
+    internal class DataLoader
+    {
+        public double[] LoadNumber(string path)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException("Soubor nebyl nalezen.", path);
+
+            var numbers = new List<double>();
+            foreach (var line in File.ReadAllLines(path))
+            {
+                var trimmed = line.Trim();
+                if (string.IsNullOrWhiteSpace(trimmed))
+                    continue;
+
+                if (double.TryParse(trimmed, System.Globalization.NumberStyles.Float,
+                                    System.Globalization.CultureInfo.InvariantCulture,
+                                    out double num))
+                    numbers.Add(num);
+                else
+                    Console.WriteLine($"Neplatná hodnota: '{line}'");
+            }
+
+            return numbers.ToArray();
+        }
+
+        public string[] LoadString(string path)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException("Soubor nebyl nalezen.", path);
+
+            var data = new List<string>();
+
+            foreach (var line in File.ReadAllLines(path))
+            {
+                var trimmed = line.Trim();
+                if (string.IsNullOrWhiteSpace(trimmed))
+                    continue;
+
+                data.Add(trimmed);
+            }
+
+            return data.ToArray();
+        }
+    }
+}
---
+++ HeapSort/Sort.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+
+namespace HeapSort
+{
+    internal class Sort
+    {
+        public T[] SortData<T>(T[] data) where T : IComparable<T>
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            int length = data.Length;
+
+            for (int i = length / 2 - 1; i >= 0; i--)
+            {
+                Heapify(data, length, i);
+            }
+
+            for (int i = length - 1; i > 0; i--)
+            {
+                T temp = data[0];
+                data[0] = data[i];
+                data[i] = temp;
+
+                Heapify(data, i, 0);
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Time: {sw.Elapsed}");
+
+            return data;
+        }
+
+        private void Heapify<T>(T[] data, int heapSize, int root) where T : IComparable<T>
+        {
+            int largest = root;
+            int left = 2 * root + 1;
+            int right = 2 * root + 2;
+
+            if (left < heapSize && data[left].CompareTo(data[largest]) > 0)
+            {
+                largest = left;
+            }
+
+            if (right < heapSize && data[right].CompareTo(data[largest]) > 0)
+            {
+                largest = right;
+            }
+
+            if (largest != root)
+            {
+                T temp = data[root];
+                data[root] = data[largest];
+                data[largest] = temp;
+
+                Heapify(data, heapSize, largest);
+            }
+        }
+    }
+}
