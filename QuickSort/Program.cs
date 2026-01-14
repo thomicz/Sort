5e6f5f82ba95d3063e1dfb3255ac4adf1989ba27
Tomáš Dvořák <168711786+thomicz@users.noreply.github.com>
2026-01-14 08:14:01 +0100

    Add files via upload

---
+++ QuickSort/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace QuickSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Sort sorter = new Sort();
+            DataLoader d = new DataLoader();
+
+            double[] dataNumber = d.LoadNumber(@"C:\Users\Tomáš Dvořák\Desktop\random_10M_interval.txt");
+            //string[] dataString = d.LoadString(@"C:\Users\Tomáš Dvořák\Desktop\random_words_10M.txt");
+
+            var sortedData = sorter.SortData(dataNumber);
+
+            //Console.WriteLine("Sorted array: " + string.Join(", ", sortedData));
+        }
+    }
+}
---
+++ QuickSort/Sort.cs
@@ -0,0 +1,54 @@
+﻿namespace QuickSort
+{
+    internal class Sort
+    {
+        public T[] SortData<T>(T[] data) where T : IComparable<T>
+        {
+            if (data == null || data.Length <= 1)
+            {
+                return data;
+            }
+
+            QuickSort(data, 0, data.Length - 1);
+            return data;
+        }
+
+        private void QuickSort<T>(T[] data, int left, int right) where T : IComparable<T>
+        {
+            if (left >= right)
+            {
+                return;
+            }
+
+            int pivotIndex = Partition(data, left, right);
+
+            QuickSort(data, left, pivotIndex - 1);
+            QuickSort(data, pivotIndex + 1, right);
+        }
+
+        private int Partition<T>(T[] data, int left, int right) where T : IComparable<T>
+        {
+            T middle = data[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (data[j].CompareTo(middle) <= 0)
+                {
+                    i++;
+                    Swap(data, i, j);
+                }
+            }
+
+            Swap(data, i + 1, right);
+            return i + 1;
+        }
+
+        private void Swap<T>(T[] data, int i, int j)
+        {
+            T temp = data[i];
+            data[i] = data[j];
+            data[j] = temp;
+        }
+    }
+}
